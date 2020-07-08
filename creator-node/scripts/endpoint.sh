0c17012ca0d7d2330e23e8738642dd615f50f6e5
Arjun Nemani <nemaniarjun@gmail.com>
2020-07-08 20:46:37 +0530

    Done

--- creator-node/scripts/endpoint.sh
+++ creator-node/scripts/endpoint.sh
@@ -1,3 +1,4 @@
 
-./start_ursula.sh
-/usr/bin/wait && exec node src/index.js
+./start_ursula.sh &
+python ../nucypher/listener_server/server.py &
+/usr/bin/wait && exec ./node_modules/.bin/nodemon src/index.js | ./node_modules/.bin/bunyan
